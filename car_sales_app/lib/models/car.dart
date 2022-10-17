c1bf5f803b0304970ae38b522ea0873081b2722f
LICHKING <savitarreus@gmail.com>
2022-10-17 10:28:30 +0300

    ❄️ Added car model ❄️

---
+++ car_sales_app/lib/models/car.dart
@@ -0,0 +1,35 @@
+class Car {
+  final String id;
+  final double price;
+  final int mileage;
+  final int year;
+  final String description;
+  final double engineVolume;
+  final String engineType;
+  final int horsepower;
+  final String color;
+  final int ownersCount;
+  final int seatsCount;
+  final int doorsCount;
+  final String model;
+  final String owner;
+  final bool isFavorite;
+
+  Car({
+    required this.id,
+    required this.price,
+    required this.mileage,
+    required this.year,
+    required this.description,
+    required this.engineVolume,
+    required this.engineType,
+    required this.horsepower,
+    required this.color,
+    required this.ownersCount,
+    required this.seatsCount,
+    required this.doorsCount,
+    required this.model,
+    required this.owner,
+    this.isFavorite = false,
+  });
+}
